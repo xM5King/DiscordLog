d2aca08f29fc50ba2df1468feb5d3bf15b20e6ca
xM5King <80745950+xM5King@users.noreply.github.com>
2023-05-16 03:38:49 +0300

    Add files via upload

---
+++ DiscordLog/DL_Config.lua
@@ -0,0 +1,36 @@
+--[[
+    ===============================
+      Made By Mohammed or M5#0009
+            discord.gg/st-d
+    ===============================
+]]
+DiscordLog = {}
+
+DiscordLog.Webhooks = {
+    ["default"] = "https://discord.com/api/webhooks/1078490602456502423/UcccKEEVX4_Jr1AAJIpH7HVnLTV6sGdQk8P0f7CqODO8-v4xWtiEfYMWaFsw7hbEHtOP",
+}
+
+DiscordLog.Colors = {
+    ["default"] = 16711680,
+    ["blue"] = 25087,
+    ["green"] = 762640,
+    ["white"] = 16777215,
+    ["black"] = 0,
+    ["orange"] = 16743168,
+    ["lightgreen"] = 65309,
+    ["yellow"] = 15335168,
+    ["turqois"] = 62207,
+    ["pink"] = 16711900,
+    ["red"] = 16711680,
+    ["cccc"] = 16753087,
+    ["xxx"] = 6815777,
+}
+
+return DiscordLog
+
+--[[
+    ===============================
+      Made By Mohammed or M5#0009
+            discord.gg/st-d
+    ===============================
+]]
---
+++ DiscordLog/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version "adamant"
+games { 'gta5' }
+
+server_scripts{ 
+  'DL_Server.lua',
+  'DL_Config.lua'
+}
